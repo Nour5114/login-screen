19bc1a077754b4146e13a0e74a5dc9438149a43c
Nour <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>
2023-01-04 15:37:19 +0200

    first version

---
+++ lib/login_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Login',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          TextFormField(
+            controller: emailController,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Email',
+              prefixIcon: Icon(Icons.email),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            controller: passwordController,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Password',
+              prefixIcon: Icon(Icons.lock),
+              suffixIcon: Icon(Icons.remove_red_eye),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'LOGIN',
+                    style: TextStyle(color: Colors.white),
+                  ))),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  [
+              const Text('Don\'t have an Account?'),
+              TextButton(onPressed: (){}, child: const Text('Register Now')),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
